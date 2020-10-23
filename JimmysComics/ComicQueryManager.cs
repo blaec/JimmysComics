9baf80315bb55be88c170056e88ead09d1dfcc6f
blaec <odeskonst@yandex.ru>
2020-10-23 23:31:18 +0300

    updates

--- JimmysComics/ComicQueryManager.cs
+++ JimmysComics/ComicQueryManager.cs
@@ -8,8 +8,10 @@ namespace JimmysComics
 {
     using System.Collections.ObjectModel;
     using System.Windows.Media.Imaging;
+
     class ComicQueryManager
     {
+
         public ObservableCollection<ComicQuery> AvailableQueries { get; private set; }
 
         public ObservableCollection<object> CurrentQueryResults { get; private set; }
@@ -33,16 +35,16 @@ namespace JimmysComics
                     "Comics whose value is over 500 bucks."
                         + " Jimmy can use this to figure out which comics are most coveted.",
                     CreateImageFromAssets("captain_amazing_250x250.jpg")),
-               
+
                 new ComicQuery("LINQ is versatile 1", "Modify every item returned from the query",
                     "This code will add a string onto the end of each string in an array.",
                     CreateImageFromAssets("bluegray_250x250.jpg")),
-            
+
                 new ComicQuery("LINQ is versatile 2", "Perform calculations on collections",
                     "LINQ provides extension methods for your collections (and anything else"
-                    + " that implements IEnumerable<T>).",
+                      + " that implements IEnumerable<T>).",
                     CreateImageFromAssets("purple_250x250.jpg")),
-           
+
                 new ComicQuery("LINQ is versatile 3",
                     "Store all or part of your results in a new sequence",
                     "Sometimes you’ll want to keep your results from a LINQ query around.",
@@ -60,6 +62,11 @@ namespace JimmysComics
                     "This query creates a list of Purchase classes that contain Jimmy's purchases"
                          + " and compares them with the prices he found on Greg's List.",
                     CreateImageFromAssets("captain_amazing_250x250.jpg")),
+
+                new ComicQuery("All comics in the collection",
+                   "Get all of the comics in the collection",
+                   "This query returns all of the comics",
+                   CreateImageFromAssets("captain_amazing_zoom_250x250.jpg")),
             };
         }
 
@@ -76,6 +83,7 @@ namespace JimmysComics
             }
         }
 
+
         public void UpdateQueryResults(ComicQuery query)
         {
             Title = query.Title;
@@ -87,31 +95,69 @@ namespace JimmysComics
                 case "LINQ is versatile 1": LinqIsVersatile1(); break;
                 case "LINQ is versatile 2": LinqIsVersatile2(); break;
                 case "LINQ is versatile 3": LinqIsVersatile3(); break;
-                case "Group comics by price range": CombineJimmysValuesIntoGroups(); break;
-                case "Join purchases with prices": JoinPurchasesWithPrices(); break;
+                case "Group comics by price range":
+                    CombineJimmysValuesIntoGroups();
+                    break;
+                case "Join purchases with prices":
+                    JoinPurchasesWithPrices();
+                    break;
+                case "All comics in the collection": AllComics(); break;
             }
         }
 
         public static IEnumerable<Comic> BuildCatalog()
         {
             return new List<Comic> {
-                new Comic { Name = "Johnny America vs. the Pinko", Issue = 6 },
-                new Comic { Name = "Rock and Roll (limited edition)", Issue = 19 },
-                new Comic { Name = "Woman’s Work", Issue = 36 },
-                new Comic { Name = "Hippie Madness (misprinted)", Issue = 57 },
-                new Comic { Name = "Revenge of the New Wave Freak (damaged)", Issue = 68 },
-                new Comic { Name = "Black Monday", Issue = 74 },
-                new Comic { Name = "Tribal Tattoo Madness", Issue = 83 },
-                new Comic { Name = "The Death of an Object", Issue = 97 },
+                new Comic { Name = "Johnny America vs. the Pinko", Issue = 6, Year = 1949, CoverPrice = "10 cents",
+                    MainVillain = "The Pinko", Cover = CreateImageFromAssets("Captain Amazing Issue 6 cover.png"),
+                    Synopsis = "Captain Amazing must save America from Communists as The Pinko and his"
+                        + " communist henchmen threaten to take over Fort Knox and steal all of the nation's gold." },
+
+                new Comic { Name = "Rock and Roll (limited edition)", Issue = 19, Year = 1957, CoverPrice = "10 cents",
+                    MainVillain = "Doctor Vortran", Cover = CreateImageFromAssets("Captain Amazing Issue 19 cover.png"),
+                    Synopsis = "Doctor Vortran wreaks havoc with the nation's youth with his radio wave device that"
+                        + " uses the latest dance craze to send rock and roll fans into a mind-control trance." },
+
+                new Comic { Name = "Woman’s Work", Issue = 36, Year = 1968, CoverPrice = "12 cents",
+                    MainVillain = "Hysterianna", Cover = CreateImageFromAssets("Captain Amazing Issue 36 cover.png"),
+                    Synopsis = "The Captain faces his first female foe, Hysterianna, whose incredible telepathic"
+                        + " and telekinetic abilities have allowed her to raise an all-girl army that"
+                                + " even the Captain has trouble resisting." },
+
+                new Comic { Name = "Hippie Madness (misprinted)", Issue = 57, Year = 1973, CoverPrice = "20 cents",
+                    MainVillain = "The Mayor", Cover = CreateImageFromAssets("Captain Amazing Issue 57 cover.png"),
+                    Synopsis = "A zombie apocalypse threatens Objectville when The Mayor rigs the election by"
+                        + " introducing his zombification agent into the city's cigarette supply." },
+
+                new Comic { Name = "Revenge of the New Wave Freak (damaged)", Issue = 68, Year = 1984,
+                    CoverPrice = "75 cents", MainVillain = "The Swindler",
+                    Cover = CreateImageFromAssets("Captain Amazing Issue 68 cover.png"),
+                    Synopsis = "A tainted batch of eye makeup turns Dr. Alvin Mudd into the Captain's new nemesis,"
+                        + " in The Swindler's first appearance in a Captain Amazing comic." },
+
+                new Comic { Name = "Black Monday", Issue = 74, Year = 1986, CoverPrice = "75 cents",
+                    MainVillain = "The Mayor", Cover = CreateImageFromAssets("Captain Amazing Issue 74 cover.png"),
+                    Synopsis = "The Mayor returns to throw Objectville into a financial crisis by directing his"
+                        + " zombie creation powers to the floor of the Objectville Stock Exchange." },
+
+                new Comic { Name = "Tribal Tattoo Madness", Issue = 83, Year = 1996, CoverPrice = "Two bucks",
+                    MainVillain = "Mokey Man", Cover = CreateImageFromAssets("Captain Amazing Issue 83 cover.png"),
+                    Synopsis = "Monkey Man escapes from his island prison and wreaks havoc with his circus sideshow"
+                        + " of tattooed henchmen that and their deadly grunge ray." },
+
+                new Comic { Name = "The Death of an Object", Issue = 97, Year = 2013, CoverPrice = "Four bucks",
+                    MainVillain = "The Swindler", Cover = CreateImageFromAssets("Captain Amazing Issue 97 cover.png"),
+                    Synopsis = "The Swindler's clone army attacks Objectville in a ruse to trap and kill the "
+                        + " Captain. Can the scientists of Objectville find a way to bring him back?" },
             };
         }
 
         private static Dictionary<int, decimal> GetPrices()
         {
             return new Dictionary<int, decimal> {
-                { 6, 3600M }, { 19, 500M }, { 36, 650M }, { 57, 13525M },
-                { 68, 250M }, { 74, 75M }, { 83, 25.75M }, { 97, 35.25M },
-            };
+            { 6, 3600M }, { 19, 500M }, { 36, 650M }, { 57, 13525M },
+            { 68, 250M }, { 74, 75M }, { 83, 25.75M }, { 97, 35.25M },
+        };
         }
 
         private void LinqMakesQueriesEasy()
@@ -208,6 +254,16 @@ namespace JimmysComics
                 CurrentQueryResults.Add(CreateAnonymousListViewItem(n.ToString(), "bluegray_250x250.jpg"));
         }
 
+        private object CreateAnonymousListViewItem(string title,
+                        string imageFilename = "purple_250x250.jpg")
+        {
+            return new
+            {
+                Title = title,
+                Image = CreateImageFromAssets(imageFilename),
+            };
+        }
+
         private void CombineJimmysValuesIntoGroups()
         {
             Dictionary<int, decimal> values = GetPrices();
@@ -261,14 +317,22 @@ namespace JimmysComics
                      totalSpent, gregsListValue);
         }
 
-        private object CreateAnonymousListViewItem(string title,
-                        string imageFilename = "purple_250x250.jpg")
+        private void AllComics()
         {
-            return new
+            foreach (Comic comic in BuildCatalog())
             {
-                Title = title,
-                Image = CreateImageFromAssets(imageFilename),
-            };
+                var result = new
+                {
+                    Image = CreateImageFromAssets("captain_amazing_zoom_250x250.jpg"),
+                    Title = comic.Name,
+                    Subtitle = "Issue #" + comic.Issue,
+                    Description = "The Captain versus " + comic.MainVillain,
+                    Comic = comic,
+                };
+                CurrentQueryResults.Add(result);
+            }
         }
+
+
     }
 }
